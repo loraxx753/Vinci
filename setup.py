c885052186679ca6e37f4792c929fd355c4c5270
Kevin Baugh <loraxx753@gmail.com>
2016-09-11 20:10:42 -0500

    updated info in the setups stuff

--- setup.py
+++ setup.py
@@ -3,21 +3,20 @@
 from setuptools import setup, find_packages
 
 
-with open('README.rst') as f:
+with open('README.md') as f:
     readme = f.read()
 
 with open('LICENSE') as f:
     license = f.read()
 
 setup(
-    name='sample',
+    name='template',
     version='0.0.1',
-    description='Sample package for Python-Guide.org',
+    description='Template Layout for Personal Projects.',
     long_description=readme,
-    author='Kenneth Reitz',
-    author_email='me@kennethreitz.com',
-    url='https://github.com/kennethreitz/samplemod',
+    author='Kevin Baugh',
+    author_email='me@kkevinbaugh.com',
+    url='https://github.com/loraxx753/python_template',
     license=license,
     packages=find_packages(exclude=('tests', 'docs'))
 )
-
